5d52566b2843e038ee8361265cf269f66bc0cb1e
tangji08 <ji.tang@polytechnique.edu>
2020-10-15 22:47:04 +0200

    new UI

--- login.py
+++ login.py
@@ -1,19 +1,26 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
 import os
+from glob import glob
 
 app = Flask(__name__)
 
 class imgs:
-    def __init__(self):
+    def __init__(self, img_paths=None):
         self.imgs = ["https://www.w3schools.com/css/img_fjords.jpg", "https://www.w3schools.com/css/img_fjords.jpg", "https://www.w3schools.com/howto/img_mountains.jpg"]
+        if img_paths is not None:
+            self.imgs = glob("{}/*jpg".format(img_paths))
         self.i = 0
     def get(self):
         self.i += 1
         if self.i >= len(self.imgs):
             self.i = 0
         return self.imgs[self.i]
+    def last(self):
+        return self.imgs[self.i]
 
-imgs_class = imgs()
+imgs_class = imgs("static/imgs")
+username = None
+log = {}
 answers = []
 
 @app.route('/')
@@ -26,7 +33,11 @@ def home():
 @app.route('/img_url')
 def get_img_url():
     answers.append(request.args.get('answer', 0, type=int))
-    return jsonify(url=imgs_class.get(), answer=answers)
+    if username not in log:
+        log[username] = {}
+    image_name = os.path.split(imgs_class.last())[1]
+    log[username][image_name] = request.args.get('answer', 0, type=int)
+    return jsonify(url=imgs_class.get())
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
@@ -40,5 +51,8 @@ def logout():
     return home()
 
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    try:
+        app.secret_key = os.urandom(12)
+        app.run(host='0.0.0.0', port=5000)
+    finally:
+        print(username, log)
