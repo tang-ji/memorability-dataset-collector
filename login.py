788ce249ecf5f2a1343c9cbf6f6e394fc70f2065
tangji08 <ji.tang@polytechnique.edu>
2020-10-16 11:45:16 +0200

    beta 0.2

--- login.py
+++ login.py
@@ -1,44 +1,69 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
 import os
+from glob import glob
 
 app = Flask(__name__)
 
-class imgs:
-    def __init__(self):
+class server:
+    def __init__(self, img_paths=None):
         self.imgs = ["https://www.w3schools.com/css/img_fjords.jpg", "https://www.w3schools.com/css/img_fjords.jpg", "https://www.w3schools.com/howto/img_mountains.jpg"]
+        if img_paths is not None:
+            self.imgs = glob("{}/*.jpg".format(img_paths))
         self.i = 0
+        self.username = None
+        self.log = {}
     def get(self):
         self.i += 1
         if self.i >= len(self.imgs):
             self.i = 0
         return self.imgs[self.i]
+    def get_all(self):
+        return self.imgs[self.i:self.i+198]
+    def last(self):
+        return self.imgs[self.i]
+    def reset(self):
+        self.i = 0
 
-imgs_class = imgs()
-answers = []
+def reset():
+    server_class.log = {}
+    server_class.reset()
 
 @app.route('/')
 def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return render_template('test.html')
+        urls = server_class.get_all()
+        return render_template('test.html', labels=urls)
 
 @app.route('/img_url')
-def get_img_url():
-    answers.append(request.args.get('answer', 0, type=int))
-    return jsonify(url=imgs_class.get(), answer=answers)
+def img_url():
+    answer = request.args.get('answer', 0, type=int);
+    if answer == 2:
+        reset()
+    else:
+        if server_class.username not in server_class.log:
+            server_class.log[server_class.username] = {}
+        image_name = os.path.split(server_class.last())[1]
+        server_class.log[server_class.username][image_name] = answer
+    return jsonify(url=server_class.get())
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-    session['username'] = request.form['username']
+    server_class.username = request.form['username']
     session['logged_in'] = True
     return home()   
 
-@app.route("/logout")
+@app.route("/logout", methods=['POST'])
 def logout():
+    reset()
     session['logged_in'] = False
     return home()
 
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    server_class = server("static/imgs")
+    try:
+        app.secret_key = os.urandom(12)
+        app.run(host='0.0.0.0', port=5000)
+    finally:
+        print(server_class.username, server_class.log)
