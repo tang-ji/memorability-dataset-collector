ac32059b886c9c55b5781057883f420d6f96aada
Ji TANG <ji.tang@polytechnique.edu>
2020-10-16 11:43:09 +0200

    Revert "update"
    This reverts commit dcaca7f1e69512bbb56fbf9cdd554737feaa2af1, reversing
    changes made to 82093242ccc2ba2f4f3adc719362f4e7aa466c1d.

--- login.py
+++ login.py
@@ -1,26 +1,20 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
 import os
-from glob import glob
 
 app = Flask(__name__)
 
-class server:
-    def __init__(self, img_paths=None):
+class imgs:
+    def __init__(self):
         self.imgs = ["https://www.w3schools.com/css/img_fjords.jpg", "https://www.w3schools.com/css/img_fjords.jpg", "https://www.w3schools.com/howto/img_mountains.jpg"]
-        if img_paths is not None:
-            self.imgs = glob("{}/*jpg".format(img_paths))
         self.i = 0
-        self.username = None
-        self.log = {}
     def get(self):
         self.i += 1
         if self.i >= len(self.imgs):
             self.i = 0
         return self.imgs[self.i]
-    def last(self):
-        return self.imgs[self.i]
-    def reset(self):
-        self.i = 0
+
+imgs_class = imgs()
+answers = []
 
 @app.route('/')
 def home():
@@ -30,21 +24,13 @@ def home():
         return render_template('test.html')
 
 @app.route('/img_url')
-def img_url():
-    answer = request.args.get('answer', 0, type=int);
-    if answer == 2:
-        server_class.log = {}
-        server_class.reset()
-    else:
-        if server_class.username not in server_class.log:
-            server_class.log[server_class.username] = {}
-        image_name = os.path.split(server_class.last())[1]
-        server_class.log[server_class.username][image_name] = answer
-    return jsonify(url=server_class.get())
+def get_img_url():
+    answers.append(request.args.get('answer', 0, type=int))
+    return jsonify(url=imgs_class.get(), answer=answers)
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-    server_class.username = request.form['username']
+    session['username'] = request.form['username']
     session['logged_in'] = True
     return home()   
 
@@ -54,9 +40,5 @@ def logout():
     return home()
 
 if __name__ == "__main__":
-    server_class = server("static/imgs")
-    try:
-        app.secret_key = os.urandom(12)
-        app.run(host='0.0.0.0', port=5000)
-    finally:
-        print(server_class.username, server_class.log)
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=5000)
