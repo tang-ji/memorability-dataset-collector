9ea4d51ac49debf270d652c78ba8753938012a89
Ji TANG <ji.tang@polytechnique.edu>
2020-10-20 11:10:40 +0200

    fix bugs

---
+++ data.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+def get_files(imgs, n_targets=66, n_filler=22, n_vigilence=12):
+    files = np.random.choice(imgs, n_targets + n_filler + n_vigilence, replace=False)
+    return files[:n_targets], files[n_targets: n_targets + n_filler], files[n_targets + n_filler: n_targets + n_filler + n_vigilence]
+    
+def next_vig(available, i, max_vigilence):
+    l = []
+    for j in range(i+1, len(available)):
+        if available[j] - available[i] > 3:
+            if available[j] - available[i] <= max_vigilence:
+                l.append(j)
+            else:
+                break
+    if len(l) == 0:
+        return None
+    return np.random.choice(l, 1)[0]
+
+def next_target(labels, target, target_gap):
+    temp_gap = 0; this_gap = 0
+    while True:
+        if labels[target + target_gap + temp_gap] == 0:
+            this_gap = target_gap + temp_gap
+            break
+        if labels[target + target_gap - temp_gap] == 0:
+            this_gap = target_gap - temp_gap
+            break
+        temp_gap += 1
+    return this_gap
+
+def set_target_reps(labels, files,file_targets, targets, target_gap):
+    for i, target in enumerate(targets):  
+        this_gap = next_target(labels, target, target_gap)
+        labels[target+this_gap] = 2
+        files[target+this_gap] = file_targets[i]
+
+def get_sequence(file_targets, file_filler, file_vigilence, target_gap=80, max_vigilence=10):
+    label_list = ["filler", "target", "target_rep", "vigilence", "vigilence_rep"]
+    n_targets, n_filler, n_vigilence = len(file_targets), len(file_filler), len(file_vigilence)
+    # shuffle
+    np.random.shuffle(file_targets)
+    np.random.shuffle(file_filler)
+    np.random.shuffle(file_vigilence)
+    n = n_targets*2 + n_filler + n_vigilence*2
+    labels = np.zeros(n)
+    files = np.array([file_targets[0]]*n)
+    targets = np.random.choice(n-target_gap, n_targets, replace=False)
+    labels[targets] = 1
+    files[targets] = file_targets
+    set_target_reps(labels, files,file_targets, targets, target_gap)
+    available = np.array(range(n))[labels == 0]
+    current_vigilence = 0
+    for i in range(n_vigilence*2):
+        s = np.random.choice(len(available), 1)[0]
+        s_next = next_vig(available, s, max_vigilence)
+        if s_next is None:
+            continue
+        labels[available[s]] = 3  
+        files[available[s]] = file_vigilence[current_vigilence]
+        labels[available[s_next]] = 4
+        files[available[s_next]] = file_vigilence[current_vigilence]
+        available = np.delete(available, s_next)
+        available = np.delete(available, s)
+        current_vigilence += 1
+        if current_vigilence == n_vigilence:
+            break
+
+    files[available] = file_filler
+    return files, labels
+
+def evaluation(labels, answers):
+    labels = np.array(labels)
+    answers = np.array(answers)
+    results = {}
+    results["correct_filler"] = sum(answers[labels==0]==0)
+    results["correct_target"] = sum(answers[labels==1]==0)
+    results["correct_target_rep"] = sum(answers[labels==2]==1)
+    results["correct_vigilence"] = sum(answers[labels==3]==0)
+    results["correct_vigilence_rep"] = sum(answers[labels==4]==1)
+    return results
+
+def print_result(n_targets, n_filler, n_vigilence, labels, answers):
+    e = evaluation(labels, answers)
+    print("filler accuracy: {:.1f}%, target accuracy: {:.1f}%, vigilence accuracy: {:.1f}%".format(e['correct_filler']/n_filler*100, 
+                                                                                               (e['correct_target']+e['correct_target_rep'])/(2*n_targets)*100, 
+                                                                                               (e['correct_vigilence']+e['correct_vigilence_rep'])/(2*n_vigilence)*100))
--- login.py
+++ login.py
@@ -1,17 +1,22 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify, make_response
 import os, pickle
 from glob import glob
+from data import *
 
 app = Flask(__name__)
 
 class server:
-    def __init__(self, img_paths=None):
-        self.imgs = ["https://www.w3schools.com/css/img_fjords.jpg", "https://www.w3schools.com/css/img_fjords.jpg", "https://www.w3schools.com/howto/img_mountains.jpg"]
-        if img_paths is not None:
-            self.imgs = glob("{}/*.jpg".format(img_paths))
+    def __init__(self, img_paths, n_targets=66, n_filler=44, n_vigilence=12):
+        self.n_targets = n_targets
+        self.n_filler = n_filler
+        self.n_vigilence = n_vigilence
+        self.n = n_targets*2 + n_filler + n_vigilence*2
+        imgs = glob("{}/*.jpg".format(img_paths))
+        file_targets, file_filler, file_vigilence = get_files(imgs, n_targets=n_targets, n_filler=n_filler, n_vigilence=n_vigilence)
+        self.imgs, self.labels = get_sequence(file_targets, file_filler, file_vigilence)
         self.i = 0
         self.username = None
-        self.log = {}
+        self.log = []
         self.logs = {}
     def login(self, username):
         self.username = username
@@ -26,7 +31,7 @@ class server:
             self.i = 0
         return self.imgs[self.i]
     def get_all(self):
-        return self.imgs[self.i:self.i+198]
+        return self.imgs[self.i:self.i+self.n]
     def last(self):
         return self.imgs[self.i]
     def reset(self):
@@ -35,7 +40,7 @@ class server:
         return("Hi {}, you have already marked {} images.".format(self.username, self.i))
 
 def reset():
-    server_class.log = {}
+    server_class.log = []
     server_class.reset()
 
 @app.route('/')
@@ -43,7 +48,7 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        labels = server_class.get_all()
+        labels = list(server_class.get_all())
         labels.append(server_class.welcome())
         return render_template('test.html', labels=labels)
 
@@ -54,9 +59,10 @@ def img_url():
         reset()
     elif answer == 3:
         server_class.logs[server_class.username] = server_class.log
-    else:
+        print_result(server_class.n_targets, server_class.n_filler, server_class.n_vigilence, server_class.labels, server_class.log)
+    elif answer == 1 or answer == 0:
         image_name = os.path.split(server_class.last())[1]
-        server_class.log[image_name] = answer
+        server_class.log.append(answer)
     return jsonify(url=server_class.get())
 
 @app.route('/login', methods=['POST'])
